9d2bf3c7e1c59673939a166cf64ad76b53c0b73b
kverchi <kverchi24@gmail.com>
2024-01-31 17:58:23 +0100

    Add sort strategy

---
+++ src/main/java/com/home/algorithms/sort/SortStrategy.java
@@ -0,0 +1,7 @@
+package com.home.algorithms.sort;
+
+import java.util.List;
+
+public interface SortStrategy {
+    void sort(List elements);
+}
---
+++ src/main/java/com/home/algorithms/sort/impl/BubbleSort.java
@@ -0,0 +1,12 @@
+package com.home.algorithms.sort.impl;
+
+import com.home.algorithms.sort.SortStrategy;
+
+import java.util.List;
+
+public class BubbleSort implements SortStrategy {
+    @Override
+    public void sort(List elements) {
+
+    }
+}
