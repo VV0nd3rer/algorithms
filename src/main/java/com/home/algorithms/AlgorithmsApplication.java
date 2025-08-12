45592c8b0a9cadce96142027beff49a602ff2ccb
kverchi <kverchi24@gmail.com>
2025-08-12 02:37:28 +0200

    Update integration test for execution time aspect

--- src/main/java/com/home/algorithms/AlgorithmsApplication.java
+++ src/main/java/com/home/algorithms/AlgorithmsApplication.java
@@ -1,8 +1,5 @@
 package com.home.algorithms;
 
-import com.home.algorithms.sort.SortStrategy;
-import com.home.algorithms.sort.impl.BubbleSort;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
--- src/test/java/com/home/algorithms/AlgorithmsApplicationTests.java
+++ src/test/java/com/home/algorithms/AlgorithmsApplicationTests.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -13,40 +14,46 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-@SpringBootTest(classes = { com.home.aop.advice.ExecutionTimeAdvice.class,
-		com.home.algorithms.sort.impl.BubbleSort.class})
+/**
+ * Specify classes to explicitly tell Spring which configuration classes to load
+ * for test’s ApplicationContext.
+ * Because @Aspect is in another package and won’t load unless explicitly imported.
+ */
+@SpringBootTest(classes = {com.home.aop.advice.ExecutionTimeAdvice.class,
+        com.home.algorithms.sort.impl.BubbleSort.class})
+@EnableAspectJAutoProxy
 @Slf4j
 class AlgorithmsApplicationTests {
 
-	@Autowired
-	SortStrategy bubbleSort;
+    @Autowired
+    SortStrategy bubbleSort;
 
-	@Test
-	void contextLoads() {
-		log.info("Hello :)");
-	}
+    @Test
+    void contextLoads() {
+        log.info("Hello :)");
+    }
 
-	@Test
-	void testBubbleSort() {
-		Integer[] array = new Integer[] {5, 9, 3};
-		Integer[] expectedArray = new Integer[] {3, 5, 9};
-		bubbleSort.sort(array);
-		assertArrayEquals(expectedArray, array);
-	}
+    @Test
+    void testBubbleSort() {
+        Integer[] array = new Integer[]{5, 9, 3};
+        Integer[] expectedArray = new Integer[]{3, 5, 9};
+        bubbleSort.sort(array);
+        assertArrayEquals(expectedArray, array);
+    }
 
-	@Test
-	void testAopExecutionTime() {
-		Integer[] array = new Integer[] {5, 9, 3};
+    @Test
+    void testAopExecutionTime() {
+        Integer[] array = new Integer[]{5, 9, 3};
 
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		PrintStream originalSystemOut = System.out;
-		System.setOut(new PrintStream(baos));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream originalSystemOut = System.out;
+        System.setOut(new PrintStream(baos));
 
-		bubbleSort.sort(array);
+        bubbleSort.sort(array);
 
-		System.setOut(originalSystemOut);
-		String logOutput = baos.toString();
-		log.info("Log output: {} ", logOutput);
-		assertThat(logOutput, containsString("Tracking execution time"));
-	}
+        System.setOut(originalSystemOut);
+        String logOutput = baos.toString();
+        log.info("Log output: {} ", logOutput);
+        assertThat(logOutput, containsString("Tracking execution time"));
+    }
 }
