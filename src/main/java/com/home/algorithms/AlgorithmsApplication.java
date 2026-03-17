88b825fb5ce85b97d283c3314bb5a8103f7368ee
kverchi <kverchi24@gmail.com>
2026-03-17 19:39:03 +0100

    Create a simple console-based application

--- src/main/java/com/home/algorithms/AlgorithmsApplication.java
+++ src/main/java/com/home/algorithms/AlgorithmsApplication.java
@@ -1,15 +1,32 @@
 package com.home.algorithms;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
-public class AlgorithmsApplication {
+public class AlgorithmsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgorithmsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Welcome in the world af algorithms, a set approaches for solving typical tasks...");
+		System.out.println("Enjoy and have fun :)");
+
+		if (args.length > 0) {
+			System.out.println("Let's start with educational " +
+					"and effective for small datasets Bubble sort algorithm." +
+					"Enter unsorted set of integer numbers: ");
+
+			for(String arg: args) {
+				System.out.println(" - " + arg);
+			}
+			System.exit(0);
+		}
+	}
 }
